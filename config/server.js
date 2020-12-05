aaa982963527d40cffeb7d01c79c2bb9e1e6a1a7
Gergő Gyulai <gergo.gyulai@debhack.tech>
2020-12-05 13:07:23 +0100

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9df533108c11c049284b4b3627bda96a'),
+    },
+  },
+});
